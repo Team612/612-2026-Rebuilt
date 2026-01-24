4cb1b386056d99008269b75e38f50ede920147e5
qwertyuiop161 <idiotmethere@gmail.com>
2026-01-24 13:51:13 -0500

    Made Motors follow each other for simplicity

--- src/main/java/frc/robot/subsystems/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -1,28 +1,25 @@
 package frc.robot.subsystems;
 
-
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
 public class TankDrive extends SubsystemBase {
-
-
   private final TalonFX leftMotor = new TalonFX(1);
   private final TalonFX rightMotor  =new TalonFX(2);
   private final TalonFX leftMotor2 = new TalonFX(3);
   private final TalonFX rightMotor2 = new TalonFX(4);
 
-
   public TankDrive() {
+    
     TalonFXConfiguration leftConfig = new TalonFXConfiguration();
     TalonFXConfiguration rightConfig = new TalonFXConfiguration();
-
+    leftMotor2.setControl(new Follower(1, MotorAlignmentValue.Aligned));
+    rightMotor2.setControl(new Follower(2, MotorAlignmentValue.Aligned));
 
     leftConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     rightConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
@@ -34,28 +31,15 @@ public class TankDrive extends SubsystemBase {
 
     leftMotor.getConfigurator().apply(leftConfig);
     rightMotor.getConfigurator().apply(rightConfig);
-    leftMotor2.getConfigurator().apply(leftConfig);
-    rightMotor2.getConfigurator().apply(rightConfig);
-   
   }
-
 
   public void arcadeDrive(double forward, double turn) {
-    double leftOutput = forward + turn;
-    double rightOutput = forward - turn;
-
-
-    leftMotor.set(leftOutput);
-    rightMotor.set(rightOutput);
-    leftMotor2.set(leftOutput);
-    rightMotor2.set(rightOutput);
+    leftMotor.set(forward + turn);
+    rightMotor.set(forward - turn);
   }
-
 
   public void stop() {
     leftMotor.stopMotor();
     rightMotor.stopMotor();
-    leftMotor2.stopMotor();
-    rightMotor2.stopMotor();
   }
 }
