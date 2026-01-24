3b6d12e39f6cdd51b6683412fcf23a0f85361b2a
Cubey <149191032+Aidnpatrick@users.noreply.github.com>
2026-01-24 13:48:40 -0500

    Removed Swerve and added an empty climb subssytem.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,6 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.ArcadeDrive;
-import frc.robot.commands.SetEncoders;
-import frc.robot.subsystems.Swerve;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
@@ -16,15 +12,12 @@ public class RobotContainer {
 
   private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-  private Swerve m_swerve = new Swerve();
 
   public RobotContainer() {
-    m_swerve.setDefaultCommand(new ArcadeDrive(m_swerve, m_driverController));
     configureBindings();
   }
 
   private void configureBindings() {
-    m_driverController.leftBumper().onTrue(new SetEncoders(m_swerve, new Pose2d()));
   }
 
   public Command getAutonomousCommand() {
