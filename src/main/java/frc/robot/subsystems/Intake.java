b2bfac458a9ff4766250c7f1d6016d1874218c5f
qwertyuiop161 <idiotmethere@gmail.com>
2026-01-31 09:57:55 -0500

    added full intake support

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,66 @@
 package frc.robot.subsystems;
 
-public class Intake {
-    
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  private static final int LEFT_MOTOR_ID = 10;
+  private static final int RIGHT_MOTOR_ID = 11;
+
+  private static final double INTAKE_SPEED = 0.85;
+  private static final double OUTTAKE_SPEED = -0.65;
+
+  private final SparkMax leftMotor;
+  private final SparkMax rightMotor;
+
+  public Intake() {
+    leftMotor = new SparkMax(LEFT_MOTOR_ID, MotorType.kBrushless);
+    rightMotor = new SparkMax(RIGHT_MOTOR_ID, MotorType.kBrushless);
+
+    SparkMaxConfig leftConfig = new SparkMaxConfig();
+    SparkMaxConfig rightConfig = new SparkMaxConfig();
+
+    leftConfig
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(40)
+        .voltageCompensation(12.0)
+        .inverted(false);
+
+    rightConfig
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(40)
+        .voltageCompensation(12.0)
+        .follow(leftMotor, true);
+
+    leftMotor.configure(
+        leftConfig,
+        SparkMax.ResetMode.kResetSafeParameters,
+        SparkMax.PersistMode.kPersistParameters);
+
+    rightMotor.configure(
+        rightConfig,
+        SparkMax.ResetMode.kResetSafeParameters,
+        SparkMax.PersistMode.kPersistParameters);
+  }
+
+  public void intake() {
+    leftMotor.set(INTAKE_SPEED);
+  }
+
+  public void outtake() {
+    leftMotor.set(OUTTAKE_SPEED);
+  }
+
+  public void stop() {
+    leftMotor.stopMotor();
+  }
+
+  @Override
+  public void periodic() {
+  }
 }
