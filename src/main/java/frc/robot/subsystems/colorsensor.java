b215c4c5e66e5ad3d8657ae60ab823e902168f5b
qwertyuiop161 <idiotmethere@gmail.com>
2026-02-09 19:30:25 -0500

    "fixing" color

--- src/main/java/frc/robot/subsystems/colorsensor.java
+++ src/main/java/frc/robot/subsystems/colorsensor.java
@@ -16,22 +16,20 @@ public class colorsensor extends TimedRobot {
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
   @Override
-  public void robotPeriodic() {
+public void robotPeriodic() {
 
+    int red = m_colorSensor.getRed();
+    int green = m_colorSensor.getGreen();
+    int blue = m_colorSensor.getBlue();
+    int ir = m_colorSensor.getIR();
 
-    Color detectedColor = m_colorSensor.getColor();
-
-
-    double IR = m_colorSensor.getIR();
-
-
-    SmartDashboard.putString("Detected Color", detectedColor.toString());
-    SmartDashboard.putNumber("IR", IR);
-
+    SmartDashboard.putNumber("Red", red);
+    SmartDashboard.putNumber("Green", green);
+    SmartDashboard.putNumber("Blue", blue);
+    SmartDashboard.putNumber("IR", ir);
 
     int proximity = m_colorSensor.getProximity();
-
     SmartDashboard.putNumber("Proximity", proximity);
+}
 
-  }
 }
