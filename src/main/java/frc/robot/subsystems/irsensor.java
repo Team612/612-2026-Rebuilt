fa1f13a794a669f9a00c47b12ad6d0f8d526ec67
achyutdipu <achyutdipu@gmail.com>
2026-02-11 15:59:13 -0500

    Updating irsensor

--- src/main/java/frc/robot/subsystems/irsensor.java
+++ src/main/java/frc/robot/subsystems/irsensor.java
@@ -2,14 +2,14 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Micro;
 
-import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SharpIR;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class irsensor extends SubsystemBase {
-  DigitalInput irSensor = new DigitalInput(0);
+  AnalogInput irSensor = new AnalogInput(0);
   SharpIR distanceSensor = SharpIR.GP2Y0A21YK0F(0);
   private final I2C.Port i2cPort;
   private final boolean m_inverted;  
@@ -31,7 +31,7 @@ public class irsensor extends SubsystemBase {
     return distanceSensor.getRangeCM()*100;
   }
   public boolean isThere() {
-    return irSensor.get();
+    return irSensor.getVoltage()> 3;
   }
   @Override
   public void periodic() {
