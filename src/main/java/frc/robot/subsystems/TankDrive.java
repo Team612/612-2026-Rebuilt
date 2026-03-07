1ec3ae28391a76871f1cb6fa8aa048f98bc29248
VictorMoldoveanu <victormoldoveanu07@gmail.com>
2026-03-07 10:55:06 -0500

    uncommented the solution that I think will work the best

--- src/main/java/frc/robot/subsystems/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -215,16 +215,16 @@ public class TankDrive extends SubsystemBase {
         }
       }
     }
-    // PhotonPipelineResult shooterResult = m_shooter.returnLatestCameraResult();
-    // if (shooterResult.hasTargets()) {
-    //   if (shooterResult.getBestTarget().getBestCameraToTarget().getX() < 4){
-    //     var estimatedPoseOptional = m_shooter.returnPhotonPos(shooterResult);
-    //     if (estimatedPoseOptional.isPresent()) {
-    //       var estimatedPose = estimatedPoseOptional.get().estimatedPose;
-    //       poseEstimator.addVisionMeasurement(estimatedPose.toPose2d(), shooterResult.getTimestampSeconds(),VecBuilder.fill(0.05, 0.05, 0.07));
-    //     }
-    //   }
-    // }
+    PhotonPipelineResult shooterResult = m_shooter.returnLatestCameraResult();
+    if (shooterResult.hasTargets()) {
+      if (shooterResult.getBestTarget().getBestCameraToTarget().getX() < 4){
+        var estimatedPoseOptional = m_shooter.returnPhotonPos(shooterResult);
+        if (estimatedPoseOptional.isPresent()) {
+          var estimatedPose = estimatedPoseOptional.get().estimatedPose;
+          poseEstimator.addVisionMeasurement(estimatedPose.toPose2d(), shooterResult.getTimestampSeconds(),VecBuilder.fill(0.05, 0.05, 0.07));
+        }
+      }
+    }
 
     // PhotonPipelineResult shooterResult = m_shooter.returnLatestCameraResult();
     // if (shooterResult.hasTargets()) {
